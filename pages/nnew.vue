eca229e99ee2db2206a5fa7c5c2ae6678e5a0e3c
dhkim <dhkim@concepters.co.kr>
2021-04-28 16:31:30 +0900

    update

--- pages/nnew.vue
+++ pages/nnew.vue
@@ -3,8 +3,6 @@
     <lazy-modules-the-header :title="$route.name" />
     <div class="contents">
       <!-- <lazy-modules-hacker-news-list class="window" /> -->
-      <div v-html="$store.state.nNewList">
-      </div>
       <lazy-modules-naver-news-list class="window2" />
     </div>
   </div>
